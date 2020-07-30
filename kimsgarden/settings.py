28ff7fc0afcdef541040077caa4771fb4c9158fa
kimebmen <simbolonkimebmen@gmail.com>
2020-07-31 01:21:50 +0800

    deploy

--- kimsgarden/settings.py
+++ kimsgarden/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'epkh!yjnpvta+=qc(8xax8!qi74tuxrmpozy=#4-j41hcdun&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['', '127.0.0.1']
 
 
 # Application definition
